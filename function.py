ee4108aac0ced60b660954e6b9c6e34eb9db355b
Info_cfg <67759901+Info4cfg@users.noreply.github.com>
2022-01-13 11:21:31 +0200

    Create function.py
    Turn with a Compass

---
+++ function.py
@@ -0,0 +1,9 @@
+def direction(facing, turn):
+    directions = ['N', 'NE', 'E', 'SE', 'S', 'SW', 'W', 'NW']
+    turns = turn // 45
+
+    start_idx = directions.index(facing)
+    end_idx = (start_idx + turns) % len(directions)
+
+    return directions[end_idx]
+    
